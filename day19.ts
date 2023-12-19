adc67310a12f1c042f8cecde418749490a73c7f1
axl0926 <song22092000@gmail.com>
2023-12-19 16:44:05 +0900

    day19

--- day19.ts
+++ day19.ts
@@ -1,3 +1,5 @@
-type Fill<T,U,Acc extends any[]=[]>=Acc['length'] extends U?Acc:Fill<T,U,[T,...Acc]>
-type AA<T extends number,B extends any[]=['🛹' ,'🚲','🛴','🏄' ]>= B[T] extends string ? B[T] : AA<T,[...B,...B]>
-type Rebuild<T,Acc extends any[]=[]> = T extends [...infer A,infer B]?Rebuild<A,[...Fill<AA<A['length']>,B>,...Acc]>:Acc;
+type Fill<Emoji,Length,Acc extends any[]=[]> = Acc['length'] extends Length ? Acc : Fill<Emoji,Length,[Emoji,...Acc]>
+
+type GetEmoji<Index extends number,EmojiArray extends any[]=['🛹' ,'🚲','🛴','🏄' ]> = EmojiArray[Index] extends string ? EmojiArray[Index] : GetEmoji<Index,[...EmojiArray,...EmojiArray]>
+
+type Rebuild<T,Acc extends any[]=[]> = T extends [...infer A,infer B] ? Rebuild<A,[...Fill<GetEmoji<A['length']>,B>,...Acc]> : Acc;
